3c1e11b3087ea491bdece199dcad3bdf022642d5
Rabi Malla <rabi.malla@outlook.com>
2025-11-25 13:07:11 +0545

    added letter_opener gem for checking sent emails in dev env

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
+  # Only mount the mailbox in the development environment
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
